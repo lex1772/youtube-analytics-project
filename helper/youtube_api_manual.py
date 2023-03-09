dd5d4a56df01fcdb3dba70d9c823e09952172ff0
lexa1 <lexa1772@gmail.com>
2023-03-09 12:04:42 +0300

    Homework done

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -8,7 +8,7 @@ import isodate
 
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-api_key: str = os.getenv('YT_API_KEY')
+api_key: str = os.getenv('YouTube-API')
 
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
@@ -26,7 +26,7 @@ docs: https://developers.google.com/youtube/v3/docs/channels/list
 сервис для быстрого получения id канала: https://commentpicker.com/youtube-channel-id.php
 '''
 # channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'  # вДудь
-channel_id = 'UC1eFXmJNkjITxPFWTy6RsWg'  # Редакция
+channel_id = 'UCr2EXJtFMunvG0CmYE3Tj8A'  # Редакция
 channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 printj(channel)
 
